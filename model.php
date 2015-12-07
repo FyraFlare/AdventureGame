16af8d9c77eee620fd318a136ae12f7c899c9ab6
Carly Cappagli <cappagli@email.arizona.edu>
2015-12-07 13:06:32 -0700

    now useing prepared statments

--- model.php
+++ model.php
@@ -16,14 +16,16 @@
 		$name = htmlspecialchars($name);
 		$pass = htmlspecialchars($pass);
 		$hash = password_hash($pass, PASSWORD_DEFAULT);
-		$check = $conn->query ("SELECT * FROM users WHERE username='".$name."';");
+		$stmt -> prepare("SELECT * FROM users WHERE username='".$name."';");
+		$check = $stmt ->execute();
 		$count = 0;
 		foreach($check as $row){
 			$count++;
 		}
 		if($count < 1){
 			$com = "INSERT INTO users VALUES ('".$name."', 1, 0, 100, 0, '".$hash."');";
-			$conn->query($com);
+			$stmt -> prepare($com);
+			$check = $stmt ->execute();
 			header("Location: login.html");
 		}
 		else{
@@ -36,14 +38,16 @@
 		$name = htmlspecialchars($name);
 		$pass = htmlspecialchars($pass);
 		$hash = password_hash($pass, PASSWORD_DEFAULT);
-		$check = $conn->query ("SELECT * FROM users WHERE username='".$name."';");
+		$stmt -> prepare("SELECT * FROM users WHERE username='".$name."';");
+		$check = $stmt ->execute();
 		foreach($check as $row){
 			$psw = $row['password'];
 		}
 		if(password_verify($pass, $psw)){
 			session_start();
 			$_SESSION['user'] = $name;
-			$check = $conn->query ("SELECT * FROM users WHERE username='".$name."';");
+			$stmt -> prepare("SELECT * FROM users WHERE username='".$name."';");
+			$check = $stmt ->execute();
 			foreach($check as $row){
 				$_SESSION['lvl'] = $row['level'];
 				$_SESSION['exp'] = $row['exp'];
@@ -67,12 +71,16 @@
 		$com = "UPDATE users SET level='".$_SESSION['lvl']."', exp='".
 			$_SESSION['exp']."'health='".$_SESSION['hp']."', story='".
 			$_SESSION['story']."' WHERE username='".$name."';";
-		$check = $conn->query ($com);
+		$stmt -> prepare($com);
+		$check = $stmt ->execute();
 	}
 
 	function getMonster($loc){
-		$count = $conn->query ("SELECT COUNT(*) FROM monsters WHERE location='".$loc."';");
-		print_r($count);
+		$stmt -> prepare("SELECT * FROM monsters WHERE location='".$loc."';");
+		$check = $stmt ->execute();
+		$array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$cont = count($array);
+		print_r($array);
 		//$chosen = rand(0, $count-1);
 		//$check = $conn->query ("SELECT * FROM monsters WHERE location='".$loc."';");
 		//$counter = 0;
@@ -85,11 +93,13 @@
 	}
 
 	function getAtackText($mons, $strength){
-		$check = $conn->query ("SELECT ".$strength." FROM monsters WHERE name='".$mons."';");
+		$stmt -> prepare("SELECT ".$strength." FROM monsters WHERE name='".$mons."';");
+		$check = $stmt ->execute();
 		foreach($check as $row){
 			$atk = $row[$strength];
 		}
-		$check = $conn->query ("SELECT text FROM atacks WHERE atack='".$atk."';");
+		$stmt -> prepare("SELECT text FROM atacks WHERE atack='".$atk."';");
+		$check = $stmt ->execute();
 		foreach($check as $row){
 			return $row['text'];
 		}
