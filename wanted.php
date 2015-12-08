5ec9921dca03ff90343f927bd24e04e487f00b93
Carly Cappagli <cappagli@email.arizona.edu>
2015-12-08 14:00:27 -0700

    this seems to not want to track wanted.php so trying again

---
+++ wanted.php
@@ -0,0 +1,39 @@
+<?php
+	//Name: Carly Cappagli
+	//Discription: 
+	require_once('model.php');# Now can call functions inside model.php
+	$want = $_POST["want"];
+	if($want == "monster"){
+		echo getMonster($_POST["place"]);
+	}
+	elseif($want == "atack"){
+		if($_POST["str"] == 1){
+			$str = 'weak';
+		}
+		else{
+			$str = 'strong';
+		}
+		echo register($_POST["mons"], $str);
+	}
+	elseif($want == "stat"){
+		echo $_POST[$_POST['stat']];
+	}
+	elseif($want == "session"){
+		if(session_status() == PHP_SESSION_NONE){
+			echo 'good';
+		}
+		else{
+			echo 'bad';
+		}
+	}
+	elseif($want == "stat"){
+		$_SESSION['lvl'] = $_POST["level"];
+		$_SESSION['exp'] = $_POST["xp"];
+		$_SESSION['hp'] =$_POST["health"];
+		$_SESSION['story'] = $_POST["sty"];
+		storeStats();
+	}
+	else{
+		echo "not working";
+	}
+?>
